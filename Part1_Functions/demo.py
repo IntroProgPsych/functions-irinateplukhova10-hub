fd36100de4519b5421a99180264e9139e26c8888
irinateplukhova10-hub <irina.teplukhova10@e-uvt.ro>
2025-11-20 13:52:21 +0000

    Demo

---
+++ Part1_Functions/demo.py
@@ -0,0 +1,15 @@
+#functions= block of reusable code
+#() - after the funcyions name
+#def - definae a function
+
+#def first_function(name, age):
+   # print(f"Hello {name}") 
+   # print("You are {age} years old")
+
+#first_function("ira")
+
+def full_name(first_name,second_name):
+  return first_name + " " + second_name
+
+print(full_name("Alex, Popescu"))
+
